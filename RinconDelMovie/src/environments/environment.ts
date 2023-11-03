6db38358ef5d0bb5c0f53753a171bc9432d919a1
Ernesto Trevino <53844061+Ernesto-Trevino-E@users.noreply.github.com>
2023-11-02 20:03:13 -0600

    Cambios del tema
    esperando a que el api key funcione

--- RinconDelMovie/src/environments/environment.ts
+++ RinconDelMovie/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '5830566668df56a73ba5e31cb5c10d3a',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
