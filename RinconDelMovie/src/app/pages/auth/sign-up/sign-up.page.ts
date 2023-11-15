87ceff67e2a257b490d5fa4110f2f0bb8a9ebf9c
Ariadne Romero <88512668+AriadneRomero@users.noreply.github.com>
2023-11-14 19:11:04 -0600

    Correcion de bugs
    Registro a Inicio

--- RinconDelMovie/src/app/pages/auth/sign-up/sign-up.page.ts
+++ RinconDelMovie/src/app/pages/auth/sign-up/sign-up.page.ts
@@ -68,7 +68,7 @@ export class SignUpPage implements OnInit {
       this.firebaseSvc.setDocument(path, this.form.value).then(async res =>{
         
         this.utilsSvc.saveInLocalStorage('user', this.form.value);
-        this.utilsSvc.routerLink('/main/home');
+        this.utilsSvc.routerLink('/auth');
         this.form.reset();
 
 
